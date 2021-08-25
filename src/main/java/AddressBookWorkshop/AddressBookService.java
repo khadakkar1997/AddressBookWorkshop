6c5f32473e54f43080d44a1f299e56cc4d068690
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-25 17:49:39 +0530

    Update AddressBookService.java

--- src/main/java/AddressBookWorkshop/AddressBookService.java
+++ src/main/java/AddressBookWorkshop/AddressBookService.java
@@ -1,37 +1,42 @@
 package AddressBookWorkshop;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
-public class AddressBookService {
+public class AddressBookService
+{
+    String firstName, lastName, address, city, state, zip, phoneNumber;
     Scanner scanner = new Scanner(System.in);
 
-    /**
-     * Purpose : To take the user input for all the details.
-     *
-     * Here all the entered details are set to the contact details in Contacts class.
-     */
-    public void createContact() {
+    LinkedList<Contacts> addressBook = new LinkedList<>();
+
+    public Contacts createContact()
+    {
         Contacts contact = new Contacts();
-        System.out.println("Enter first name : ");
+        System.out.println("Enter first name :: ");
         contact.setFirstName(scanner.next());
-        System.out.println("Enter last name : ");
+        System.out.println("Enter last name :: ");
         contact.setLastname(scanner.next());
-        System.out.println("Enter address : ");
+        System.out.println("Enter address :: ");
         contact.setAddress(scanner.next());
-        System.out.println("Enter city : ");
+        System.out.println("Enter city :: ");
         contact.setCity(scanner.next());
-        System.out.println("Enter state : ");
+        System.out.println("Enter state :: ");
         contact.setState(scanner.next());
-        System.out.println("Enter zip : ");
-        contact.setZip(scanner.nextLong());
-        System.out.println("Enter phone number : ");
-        contact.setPhoneNumber(scanner.nextLong());
-        System.out.println("Enter Email :");
+        System.out.println("Enter zip :: ");
+        contact.setZip(scanner.next());
+        System.out.println("Enter phone number :: ");
+        contact.setPhoneNumber(scanner.nextInt());
+        System.out.println("Enter Email ::");
         contact.setEmail(scanner.next());
         scanner.close();
 
-        System.out.println(contact);
+        return contact;
 
     }
-
+    public LinkedList<Contacts> addContacts(Contacts contact)
+    {
+        addressBook.add(contact);
+        return addressBook;
+    }
 }
