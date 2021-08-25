f8112d6d069afcba5cdc26b9507d2addb4150ab7
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-25 18:01:09 +0530

    Create CustomException.java

---
+++ src/main/java/AddressBookWorkshop/CustomException.java
@@ -0,0 +1,18 @@
+   
+package AddressBookWorkshop;
+
+public class CustomException extends Exception
+{
+    public enum ExceptionsType
+    {
+        EMPTY_LIST, WRONG_INPUT, NUll_VALUE
+    }
+
+    public ExceptionsType type;
+
+    public CustomException(ExceptionsType type, String message)
+    {
+        super(message);
+        this.type = type;
+    }
+}
