97c337af9d93ebd19308db8e154f83e330babf79
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-25 17:58:02 +0530

    Update AddressBookService.java

--- src/main/java/AddressBookWorkshop/AddressBookService.java
+++ src/main/java/AddressBookWorkshop/AddressBookService.java
@@ -1,42 +1,98 @@
 package AddressBookWorkshop;
 
-import java.util.LinkedList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class AddressBookService
 {
-    String firstName, lastName, address, city, state, zip, phoneNumber;
+    Contacts[] addressBook = new Contacts[20];
+    private int index = 0;
     Scanner scanner = new Scanner(System.in);
 
-    LinkedList<Contacts> addressBook = new LinkedList<>();
-
-    public Contacts createContact()
+    public Contacts createContact() throws CustomException
     {
         Contacts contact = new Contacts();
-        System.out.println("Enter first name :: ");
-        contact.setFirstName(scanner.next());
-        System.out.println("Enter last name :: ");
-        contact.setLastname(scanner.next());
-        System.out.println("Enter address :: ");
-        contact.setAddress(scanner.next());
-        System.out.println("Enter city :: ");
-        contact.setCity(scanner.next());
-        System.out.println("Enter state :: ");
-        contact.setState(scanner.next());
-        System.out.println("Enter zip :: ");
-        contact.setZip(scanner.next());
-        System.out.println("Enter phone number :: ");
-        contact.setPhoneNumber(scanner.nextInt());
-        System.out.println("Enter Email ::");
-        contact.setEmail(scanner.next());
-        scanner.close();
-
-        return contact;
-
+        return getDetails(contact);
     }
-    public LinkedList<Contacts> addContacts(Contacts contact)
+
+    private Contacts getDetails(Contacts contact) throws CustomException
     {
-        addressBook.add(contact);
+        try
+        {
+            System.out.println("Enter First Name ::");
+            contact.setFirstName(scanner.next());
+            System.out.println("Enter last Name ::");
+            contact.setLastname(scanner.next());
+            System.out.println("Enter address ::");
+            contact.setAddress(scanner.next());
+            System.out.println("Enter City ::");
+            contact.setCity(scanner.next());
+            System.out.println("Enter State ::");
+            contact.setState(scanner.next());
+            System.out.println("Enter Pincode ::");
+            contact.setZip(scanner.next());
+            System.out.println("Enter Phone Number ::");
+            contact.setPhoneNumber(scanner.nextInt());
+            System.out.println("Enter Email ::");
+            contact.setEmail(scanner.next());
+            return contact;
+        } catch (InputMismatchException e)
+        {
+            throw new CustomException(CustomException.ExceptionsType.WRONG_INPUT, "Entered the wrong value");
+        }
+    }
+
+    public Contacts[] addContacts(Contacts contact) throws CustomException
+    {
+        try
+        {
+            addressBook[index++] = contact;
+            return addressBook;
+        } catch (Exception e)
+        {
+            throw new CustomException(CustomException.ExceptionsType.NUll_VALUE, "Obtained value is null");
+        }
+    }
+
+    public Contacts[] editContact(String name) throws CustomException
+    {
+        boolean is_found = false;
+        if (addressBook.length == 0)
+        {
+            throw new CustomException(CustomException.ExceptionsType.EMPTY_LIST, "AddessBook is empty");
+        } else
+        {
+            for (int contact = 0; contact < addressBook.length; contact++)
+            {
+                if (addressBook[contact] != null && addressBook[contact].getFirstName().equals(name))
+                {
+                    getDetails(addressBook[contact]);
+                    is_found = true;
+                    System.out.println("Contact Updated");
+                }
+            }
+        }
+        if (!is_found)
+        {
+            System.out.println("contact not found");
+        }
         return addressBook;
+    }
+
+    public void displayContacts()
+    {
+        if (addressBook != null)
+        {
+            for (int contact = 0; contact < addressBook.length; contact++)
+            {
+                if (addressBook[contact] != null)
+                {
+                    System.out.println(addressBook[contact]);
+                }
+            }
+        } else
+        {
+            System.out.println("Contact list is empty");
+        }
     }
 }
