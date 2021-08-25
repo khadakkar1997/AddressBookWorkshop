1cc4b391dc74cbe1ca25e4473a3b959a54eabf42
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-25 17:46:10 +0530

    Update Contacts.java

--- src/main/java/AddressBookWorkshop/Contacts.java
+++ src/main/java/AddressBookWorkshop/Contacts.java
@@ -1,104 +1,106 @@
 package AddressBookWorkshop ;
-public class Contacts
-{
+
+public class Contacts {
     private String firstName;
     private String lastname;
     private String address;
     private String city;
     private String state;
-    private String zip;
-    private int phoneNumber;
+    private long zip;
+    private long phoneNumber;
     private String email;
 
-    public Contacts()
-    {
+    public Contacts() {
     }
 
-    public String getFirstName()
-    {
+    // Parametrised constructor for Contacts.
+    public Contacts(String firstName, String lastname, String address, String city, String state, long zip, long phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    // Generating getters and setters for Contacts.
+    public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName)
-    {
+    public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
-    public String getLastname()
-    {
+    public String getLastname() {
         return lastname;
     }
 
-    public void setLastname(String lastname)
-    {
+    public void setLastname(String lastname) {
         this.lastname = lastname;
     }
 
-    public String getAddress()
-    {
+    public String getAddress() {
         return address;
     }
 
-    public void setAddress(String address)
-    {
+    public void setAddress(String address) {
         this.address = address;
     }
 
-    public String getCity()
-    {
+    public String getCity() {
         return city;
     }
 
-    public void setCity(String city)
-    {
+    public void setCity(String city) {
         this.city = city;
     }
 
-    public String getState()
-    {
+    public String getState() {
         return state;
     }
 
-    public void setState(String state)
-    {
+    public void setState(String state) {
         this.state = state;
     }
 
-    public String getZip()
-    {
+    public long getZip() {
         return zip;
     }
 
-    public void setZip(String zip)
-    {
+    public void setZip(long zip) {
         this.zip = zip;
     }
 
-    public int getPhoneNumber()
-    {
+    public long getPhoneNumber() {
         return phoneNumber;
     }
 
-    public void setPhoneNumber(int phoneNumber)
-    {
+    public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
-    public String getEmail()
-    {
+    public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email)
-    {
+    public void setEmail(String email) {
         this.email = email;
     }
 
     @Override
-    public String toString()
-    {
-        return "Contacts [firstName=" + firstName + ", lastname=" + lastname + ", address=" + address + ", city=" + city
-                + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", email=" + email + "]";
+    public String toString() {
+        return "Contacts{" +
+                "firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip=" + zip +
+                ", phoneNumber=" + phoneNumber +
+                ", email='" + email + '\'' +
+                '}';
     }
-
 }
